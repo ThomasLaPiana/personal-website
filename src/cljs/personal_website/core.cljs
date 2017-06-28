2214df612b85558118a6fc42cbdab926c5b6acdf
Thomas La Piana <tal103020@icloud.com>
2017-06-28 16:13:56 -0500

    i had it working and then i broke it whoops

--- src/cljs/personal_website/core.cljs
+++ src/cljs/personal_website/core.cljs
@@ -1,30 +1,66 @@
 (ns personal-website.core
   (:require
-   [reagent.core :as reagent]))
+   [reagent.core :as r]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Content
+
+(defn music-content []
+  [:div
+   [:iframe {:width "100%"
+             :height "450"
+             :scrolling "no"
+             :frameborder "no"
+             :src "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/254096186&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"}]])
+
+(defn coding-content []
+  [:div
+   [:p "Test"]])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Vars
-
+(defonce body-content (r/atom music-content))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Home Page
 
+(defn content-box [val]
+  [:div.content-box
+   [:p "This is a content box"]])
+
 (defn header-component []
   [:div
-   [:h1 "About Me"]])
+   [:h1 "Thomas La Piana"]])
+
+(defn music-column []
+  (let [val "Mixing"]
+  [:div.column-left
+   [:h3.center {:style {:cursor "pointer"}
+                :on-click #(swap! body-content music-content)}val]]))
+
+(defn coding-column []
+  (let [val "Coding"]
+  [:div.column-center
+   [:h3.center {:style {:cursor "pointer"}
+                :on-click #(swap! body-content coding-content)}val]]))
+
+(defn gaming-column []
+  [:div.column-right
+   [:h3.center "I play a lot of Games"]])
 
 (defn body-component []
   [:div
-   [:p "Things I like include:"
-    [:ul
-     [:li "Music"]
-     [:li "Netflix"]
-     [:li "Gaming"]]]])
+   [:h2
+    {:style {:color "black",:text-align "center"}} "Things I do:"]
+    [:div
+     [music-column]
+     [coding-column]
+     [gaming-column]]
+   [:div [body-content]]])
 
 (defn footer-component []
   [:div
-   [:p "We chillin for now"]])
+   [:p.center "We chillin for now"]])
 
 (defn home-page []
   [:div
@@ -42,7 +78,7 @@
     ))
 
 (defn reload []
-  (reagent/render [home-page]
+  (r/render [home-page]
                   (.getElementById js/document "app")))
 
 (defn ^:export main []
