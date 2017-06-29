7b3d409e011061f37f47802ee2e7667146e5664c
Thomas La Piana <tal103020@icloud.com>
2017-06-29 16:18:11 -0500

    added documentation to the existing functions

--- src/cljs/personal_website/core.cljs
+++ src/cljs/personal_website/core.cljs
@@ -3,7 +3,7 @@
    [reagent.core :as r]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Content
+;; Define the content blocks
 
 (defn music-content []
   [:div
@@ -23,25 +23,32 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Vars
+
+;; Define the initial app-state
 (defonce app-state
   (r/atom
      {:active-column "Mixing"}))
 
+
+;; Create a hash-map that contains the keys for each title and their content
 (def content-map
   {:Mixing music-content
    :Coding coding-content
    :Gaming gaming-content})
 
+;; List that eventually created the clickable headers
 (def header-list ["Mixing", "Coding", "Gaming"])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Home Page
 
 (defn title-component []
+  "Page title"
   [:div
    [:h1 "Thomas La Piana"]])
 
 (defn column-header [column-title]
+  "Function that creates the headers"
   (let [val column-title]
   [:div.column-header
    [:h3.center {:style {:cursor "pointer"}
@@ -49,17 +56,21 @@
                 :on-click #(swap! app-state assoc-in [:active-column] val)}val]]))
 
 (defn header-component []
+  "Function that sets up the headers"
   [:div
    [:h2 {:style {:color "black",:text-align "center"}} "Things I do:"]
    [:div
      (for [title header-list]
       ^{:key title} [column-header title])]])
 
+
 (defn content-component []
+  "Function that displays the content"
   [:div.content-box
    [(get content-map (keyword (:active-column @app-state)))]])
 
 (defn app[]
+  "Function that contains all of the components"
   [:div
    [title-component]
    [header-component]
